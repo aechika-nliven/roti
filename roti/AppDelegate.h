c67c9f873c995641eb8ce3ee4f3ae33a881fbfef
Anthony Chika <anthony.chika@nliven.com>
2014-12-17 13:30:11 -0800

    Started
    Signed-off-by: Anthony Chika <anthony.chika@nliven.com>

---
+++ roti/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  roti
+//
+//  Created by Anthony Chika on 12/17/14.
+//  Copyright (c) 2014 Anthony Chika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ roti/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  roti
+//
+//  Created by Anthony Chika on 12/17/14.
+//  Copyright (c) 2014 Anthony Chika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
